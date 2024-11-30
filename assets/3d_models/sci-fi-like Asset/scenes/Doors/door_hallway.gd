ab96397b402cb2c6b61bb0afd40a3a512be81758
Martin Nicholson <recyclebing+github@gmail.com>
2024-11-30 13:55:54 +0100

    big assets add

---
+++ assets/3d_models/sci-fi-like Asset/scenes/Doors/door_hallway.gd
@@ -0,0 +1,22 @@
+extends MeshInstance3D
+
+@onready var animation = $AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_3d_body_entered(body):
+	if body is CharacterBody3D:
+		animation.play("Open")
+
+
+func _on_area_3d_body_exited(body):
+	if body is CharacterBody3D:
+		animation.play_backwards("Open")
+
